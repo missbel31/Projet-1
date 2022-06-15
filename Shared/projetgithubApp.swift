d9a61a1add1261df4e085f8a1708ca43321c452d
Apprenant 15 <apprenant15@macbook-pro-de-apprenant.home>
2022-06-15 15:26:14 +0200

    first commit

---
+++ Shared/projetgithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  projetgithubApp.swift
+//  Shared
+//
+//  Created by Apprenant 15 on 15/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct projetgithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
